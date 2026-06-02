2b4bd895e34d01014ff8309543b02ebcf28e813a
Songpiano <songpiano@users.noreply.github.com>
2026-06-02 17:01:49 +0900

    Move manager to right column of card

--- js/render.js
+++ js/render.js
@@ -85,10 +85,7 @@ function renderParts() {
             <span id="field_${p.id}_name">${escHtml(p.name)}</span>
             <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"><path d="M11 4H4a2 2 0 00-2 2v14a2 2 0 002 2h14a2 2 0 002-2v-7"/><path d="M18.5 2.5a2.121 2.121 0 013 3L12 15l-4 1 1-4 9.5-9.5z"/></svg>
           </div>
-          <div class="part-code-row">
-            <div class="part-code"># ${escHtml(p.code)}</div>
-            ${p.manager ? `<div class="part-manager">👤 ${escHtml(p.manager)}</div>` : ''}
-          </div>
+          <div class="part-code"># ${escHtml(p.code)}</div>
         </div>
         <div class="part-specs-right">
           <div class="spec-inline-group">
@@ -124,6 +121,10 @@ function renderParts() {
           </div>
         </div>
       </div>
+      ${p.manager ? `<div class="part-manager-col">
+        <div class="part-manager-label">담당자</div>
+        <div class="part-manager-name">${escHtml(p.manager)}</div>
+      </div>` : ''}
     </div>`;
   }).join('');
 }
